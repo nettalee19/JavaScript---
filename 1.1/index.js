1e58376db78beeb5a19f484d2969234ffc461f92
Netta Avidan <nettavidan19@gmail.com>
2021-02-07 11:09:24 +0200

    Initial commit

---
+++ 1.1/index.js
@@ -0,0 +1,29 @@
+(function(){
+  'use strict';
+  
+  // var readlineSync = require('readline-sync');
+
+  let string = 'hello';
+  let num = 4;
+  let person = {
+    firstName: "Netta",
+    lastName: "Avidan",
+    age: 30
+  };
+  
+  let isTrue = true;
+
+  const num2 = null;
+  let undi = undefined;
+  let symb = '!';
+
+  console.log(string);
+  console.log(num);
+  console.log(person.firstName, person.lastName, person.age);
+  console.log(isTrue);
+  console.log(num2);
+  console.log(undi);
+  console.log(symb);
+
+ 
+})();
