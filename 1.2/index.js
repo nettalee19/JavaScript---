d18d986a71c62697fd3ae988f2089ea89075070e
Netta Avidan <nettavidan19@gmail.com>
2021-02-07 11:36:35 +0200

    1.2

--- 1.2/index.js
+++ 1.2/index.js
@@ -14,11 +14,9 @@ b) false
 
 4. What is the result of 80 + 71.2?
 a) 151.2
-b) 151
-c) 8071.2
+
 5. What is the result of "80" + 71.2?
-a) 151.2
-b) 151
+
 c) "8071.2"
 6. Does 100 + 30 produce a number or a string?
 
@@ -31,10 +29,15 @@ b)string
 //------Submit your answers under the question------//
 
 //create three different ways to declare variables
+const
+let
+var
 
 //declare a variable and reassign a value to it
+let a = 5;
 
 //create a variable and after that assign a string with its value being: He's got it!
+let string1 = "He's got it!";
 
 /*
 1. create a variable and assign a value on how much a restaurant bill is.
@@ -42,6 +45,16 @@ b)string
 3. create a variable that will calculate the bill * tax and its output would be: Your total bill is <total bill> $.
  */
 
+ let bill = 27;
+ let tax = 2;
+ let total = bill * tax;
+ console.log ("Your total bill is " + total + " $");
+
 // Round the number 50.6 to its nearest integer
 
+let num = 50.6;
+let numFixed = round(num); 
+
 //Create a variable that is undefined
+
+let undi = undefined;
