ad2c12d0721e54f8490d37ff5a794f4828d96901
Netta Avidan <nettavidan19@gmail.com>
2021-02-08 13:27:41 +0200

    4.4

---
+++ 4.4/4.4.js
@@ -0,0 +1,32 @@
+function sevenBoomAll(num){
+  for(let i=1; i<num; i++){
+      console.log(i);
+  }
+}
+
+function sevenBoom(num){
+  for(let i=1; i <= num; i++){
+
+     if( (i % 7 === 0) && ((i % 10 === 7) || (((i/10) - (Math.floor(i/10))) === 7) )){
+       console.log("BOOM-BOOM");
+     }
+      
+    else if( (i % 7 === 0) ){
+        console.log("BOOM");
+    }
+
+      else{
+          console.log(i); 
+      }
+  }
+}
+
+
+
+
+
+ //sevenBoom(18);
+
+// sevenBoom(50);
+
+  sevenBoom(99);
