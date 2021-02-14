f3670c13439fd0e48db4e2d0e93bb2a8e661d782
Netta Avidan <nettavidan19@gmail.com>
2021-02-14 15:58:16 +0200

    9.2

--- 9.2/9.2.js
+++ 9.2/9.2.js
@@ -1,8 +1,31 @@
 const numbers = [1, -5, 666, 2, 400, 11];
 
-numbers.sort();
+// numbers.sort();
+// console.log(numbers);
+
+
+// numbers.sort().reverse();
+// console.log(numbers);
+
+
+numbers.sort(function (a, b) {
+  if (b > a) {
+      return -1;
+  }
+  else if (a > b) {
+      return 1;
+  }
+  return 0;
+});
 console.log(numbers);
 
-
-numbers.sort().reverse();
+numbers.sort(function (a, b) {
+  if (a > b) {
+      return -1;
+  }
+  else if (b > a) {
+      return 1;
+  }
+  return 0;
+});
 console.log(numbers);
