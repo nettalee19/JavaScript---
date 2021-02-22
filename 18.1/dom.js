3d21890c81b0a6df53f9ce5ce7fc1cbf6a61dace
Netta Avidan <nettavidan19@gmail.com>
2021-02-22 21:22:24 +0200

    18.1

--- 18.1/dom.js
+++ 18.1/dom.js
@@ -2,16 +2,36 @@ let text = document.querySelector("#text")
 let btn = document.querySelector("#btn")
 let smiley = document.querySelector("#smiley")
 
-// let img = document.createElement("img");
-//     img.src = ;
+
 
 
 btn.addEventListener('click', () =>{
+  let hasImg = document.querySelectorAll("IMG")
+  if(hasImg){
+    hasImg.forEach(img => {
+      img.remove()
+    })
+  }
+
   let num = document.querySelector('#text').value
-  //console.log(num)
-  //for(let i=0; i <num ; i++){
-    //console.log("hello")
-    smiley.innerHTML = "&#128525".repeat(num)
-  //}
+ 
+  if (num === 0 ){
+    smily.innerHTML = "No such thing as 0 smilies"
+  }
+  else{
+    for(let i=0; i <num ; i++){
+      console.log("hello")
+  
+      let x = document.createElement("IMG");
+      x.setAttribute("src", "tenor.gif");
+      x.setAttribute("width", "100");
+      x.setAttribute("height", "100");
+      x.setAttribute("alt", "The Pulpit Rock");
+      document.body.appendChild(x);
+  
+      
+    }
+  }
+  
     
 })
