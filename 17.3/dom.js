d73de45917642f1bbd5210b82aeef28e27b899b1
Netta Avidan <nettavidan19@gmail.com>
2021-02-22 15:27:23 +0200

    17.3

---
+++ 17.3/dom.js
@@ -0,0 +1,7 @@
+const myFirstDiv = document.querySelector("#myFirstDiv");
+const mySecondDiv = document.querySelector("#mySecondDiv");
+const myThirdDiv = document.querySelector("#myThirdDiv");
+const myFourthDiv = document.querySelector("#myFourthDiv");
+
+
+const divs =  document.querySelectorAll("#myFirstDiv","#mySecondDiv","#myThirdDiv","#myFourthDiv")
