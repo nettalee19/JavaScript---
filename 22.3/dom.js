abc1baab2b81e01b83b3794ea976897750cb57ab
Netta Avidan <nettavidan19@gmail.com>
2021-02-24 11:48:28 +0200

    22.3

--- 22.3/dom.js
+++ 22.3/dom.js
@@ -1,32 +1,38 @@
 let letter = "abcdefghijklmnopqrstuvwxyz"
 let chosenLetter = letter[Math.floor(Math.random()*letter.length)]; 
+
+
 let message = document.querySelector(".message")
-
-
 let input = document.querySelector(".user-answer")
 let playerGuesses = []
+let guessesArray = document.querySelector(".guesses-array")
+
 //console.log(input.value)
 
 input.addEventListener("keyup", ()=> {
   console.log(chosenLetter)
+
   if(input.value === chosenLetter){
-    //console.log(chosenLetter)
-    message.innerHTML = "Correct guess!"
-    message.style.color = "green";
+      //console.log(chosenLetter)
+      message.innerHTML = "Correct letter!"
+      message.style.color = "green";
+      guessesArray.remove()
   }
-  else if(input.value !== chosenLetter){
-    message.innerHTML = "Wrong guess!"
-    message.style.color = "red";
+ 
+  else{  //(input.value !== chosenLetter){
+    
     console.log(playerGuesses)
-    if(playerGuesses.includes(input.value)){
-      message.innerHTML = "You already guesses that letter"
+
+    if(!playerGuesses.includes(input.value)){
+      message.innerHTML = "Wrong guess!"
       message.style.color = "red";
+      playerGuesses.push(input.value)
+      guessesArray.innerHTML = `${playerGuesses}`//[input.value]
     }
     else{
-      playerGuesses.push(input.value)
+      message.innerHTML = "You already guessed that letter"
+      message.style.color = "red";
     }
   }
-  else{
-    message.remove()
-  }
+  
 })
