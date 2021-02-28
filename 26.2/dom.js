306d8004614a821c58210baab1411ce5a4bdd3b1
Netta Avidan <nettavidan19@gmail.com>
2021-02-28 12:19:33 +0200

    26.2

--- 26.2/dom.js
+++ 26.2/dom.js
@@ -1,8 +1,10 @@
 const storm = {
-  // add code here
+ superPower: "flying",
+ printSuperPower : printSuperPower,
 }
 
 function printSuperPower() {
   console.log("my superpower is " + this.superPower);
   
 }
+storm.printSuperPower()
