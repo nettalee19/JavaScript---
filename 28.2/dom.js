9ca5028df7027bc0f9cca095e6872ed1991828ed
Netta Avidan <nettavidan19@gmail.com>
2021-02-28 18:45:36 +0200

    28.2

--- 28.2/dom.js
+++ 28.2/dom.js
@@ -4,15 +4,11 @@ let person = {
     console.log(`hello ${this.name}`)
   },
   umm: function(){
-    setTimeout(console.log(`hello ${this.name}`), 1000)
+    setTimeout(function(){
+      console.log(`hello ${this.name}`)
+    }.bind(this), 1000)
   }
 }
 
-// let me = {
-//   name:'netta'
-// }
-
 person.sayName();
 person.umm();
-//sayName.call(me)
-//me.sayName()
