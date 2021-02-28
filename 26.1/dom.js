31016fed69d7b3c515c55e6f6fc77266e0dcddd1
Netta Avidan <nettavidan19@gmail.com>
2021-02-28 12:16:11 +0200

    26.1

--- 26.1/dom.js
+++ 26.1/dom.js
@@ -9,38 +9,46 @@
   //
   const myObj = {
     name: "Timmy",
-    greet: () => {
+    greet: function () {
     console.log(`Hello ${this.name}`);
     },
   }; 
   myObj.greet();
   
-  const myObj = {
-    name: "Timmy",
-    greet() {
-      console.log(`Hello ${this.name}`);
-      }
-  }; 
-  myObj.greet();
-  //const greetPerson = new myObj('Timmy')
-//})
 
-//print the window because this is a function
-const myFuncDec = function () {
-  console.log(this);
-};
 
-//print the window because this is a function
-const myFuncArrow = () => {
-  console.log(this);
-}; 
-myFuncArrow();
 
-//this is a callback function which reffers to the global scope. 
-document.querySelector(".element").addEventListener(() => {
-  console.log(this);
-});
+//   const myObj = {
+//     name: "Timmy",
+//     greet() {
+//       console.log(`Hello ${this.name}`);
+//       }
+//   }; 
+//   myObj.greet();
+//   //const greetPerson = new myObj('Timmy')
+// //})
 
-document.querySelector(".element").addEventListener(() => {
-  console.log(this);
-}, this);
+// //print the window because this is a function
+// const myFuncDec = function () {
+//   console.log(this);
+// };
+
+
+
+// //print the window because this is a function
+// const myFuncArrow = () => {
+//   console.log(this);
+// }; 
+// myFuncArrow();
+
+
+
+
+// //this is a callback function which reffers to the global scope. 
+// document.querySelector(".element").addEventListener(() => {
+//   console.log(this);
+// });
+
+// document.querySelector(".element").addEventListener(() => {
+//   console.log(this);
+// }, this);
