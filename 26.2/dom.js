7f2323af2d5c86e9a1e1e28dbe7708c388f3a55c
Netta Avidan <nettavidan19@gmail.com>
2021-02-28 11:00:23 +0200

    26.1

---
+++ 26.2/dom.js
@@ -0,0 +1,26 @@
+//let hello = document.querySelector(".hello")
+
+///hello.addEventListener("click", ()=>{
+
+  //it will console the window
+  console.log(this)
+  
+  
+  //
+  const myObj = {
+    name: "Timmy",
+    greet: () => {
+    console.log(`Hello ${this.name}`);
+    },
+  }; 
+  myObj.greet();
+  
+  const myObj = {
+    name: "Timmy",
+    greet() {
+      console.log(`Hello ${this.name}`);
+      }
+  }; 
+  myObj.greet();
+  //const greetPerson = new myObj('Timmy')
+//})
